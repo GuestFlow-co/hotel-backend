e47033efae1f794812084c567c5adacd0886ebd7
jadaan96 <jadaan.2009@gmail.com>
2023-08-02 20:10:25 +0000

    edit the user

--- src/models/users.js
+++ src/models/users.js
@@ -10,12 +10,9 @@ const userModel = (sequelize, DataTypes) => {
     username: { type: DataTypes.STRING, allowNull:false, unique: true },
     password: { type: DataTypes.STRING, allowNull:false},
     role: {
-      type: DataTypes.ENUM("customers","Employees", "admin"),
+      type: DataTypes.ENUM("user", "admin"),
       allowNull:false,
-      defaultValue: "customers",
-    },
-    email:{
-      type:{DataTypes:STRING}
+      defaultValue: "user",
     },
     token: {
       type: DataTypes.VIRTUAL,
