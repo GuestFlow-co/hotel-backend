5f0c2006a3fcd3c43d56ec1cb1a0990489931644
saleh2001k <saleh.ayman2001@gmail.com>
2023-08-06 14:56:33 +0300

    password res fixed

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,42 +1,57 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const authRouter = express.Router();
 
-const { users } = require('../models');
-const basicAuth = require('../auth/basic.js')
-const bearerAuth = require('../auth/bearer.js')
-const permissions = require('../auth/acl.js')
+const { users } = require("../models");
+const basicAuth = require("../auth/basic.js");
+const bearerAuth = require("../auth/bearer.js");
+const permissions = require("../auth/acl.js");
 
-authRouter.post('/signup', async (req, res, next) => {
+authRouter.post("/signup", async (req, res, next) => {
   try {
     let userRecord = await users.create(req.body);
     const output = {
       user: userRecord,
-      token: userRecord.token
+      token: userRecord.token,
     };
     res.status(201).json(output);
   } catch (e) {
-    next(e.message)
+    next(e.message);
   }
 });
 
-authRouter.post('/signin', basicAuth, (req, res, next) => {
+authRouter.post("/signin", basicAuth, (req, res, next) => {
   const user = {
-    user: req.user,
-    token: req.user.token
+    user: {
+      user_id: req.user.user_id,
+      firstName: req.user.firstName,
+      lastName: req.user.lastName,
+      email: req.user.email,
+      phoneNumber: req.user.phoneNumber,
+      username: req.user.username,
+      role: req.user.role,
+      updatedAt: req.user.updatedAt,
+      createdAt: req.user.createdAt,
+    },
+    token: req.user.token,
   };
   res.status(200).json(user);
 });
 
-authRouter.get('/users', bearerAuth, permissions('delete'), async (req, res, next) => {
+authRouter.get(
+  "/users",
+  bearerAuth,
+  permissions("delete"),
+  async (req, res, next) => {
     const userRecords = await users.model.findAll({});
-    const list = userRecords.map(user => user.username);
+    const list = userRecords.map((user) => user.username);
     res.status(200).json(list);
-  });
+  }
+);
 
-authRouter.get('/secret', bearerAuth, async (req, res, next) => {
-  res.status(200).send('Welcome to the secret area')
+authRouter.get("/secret", bearerAuth, async (req, res, next) => {
+  res.status(200).send("Welcome to the secret area");
 });
 
 module.exports = authRouter;
--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -1,11 +1,15 @@
 "use strict";
 
 const express = require("express");
-const model=require("../models/index")
+const model = require("../models/index");
 const data = require("../models/index");
 const modelsMiddleware = require("../middlewares/modelsMiddleware");
-const {RoomFeatureModel,EmployeeRoleModel,EmployeeModel}=require('../models/index')
-const CustomerModel = require('../models/index');
+const {
+  RoomFeatureModel,
+  EmployeeRoleModel,
+  EmployeeModel,
+} = require("../models/index");
+const CustomerModel = require("../models/index");
 const router = express.Router();
 
 router.param("model", modelsMiddleware);
@@ -15,113 +19,89 @@ router.get("/:model/:id", handleGetOne);
 router.post("/:model", handleCreate);
 router.put("/:model/:id", handleUpdate);
 router.delete("/:model/:id", handleDelete);
-// router.get("/:model/getAllRooms/:id", handleAllRooms)
 
-
-// async function handleAllRooms(req, res){
-//   const id = req.params.id;
-//   const record= await req.model.readAll(id, model.RoomModel)
-//   res.status(200).json(record);
-
-// }
 async function handleGetAll(req, res, next) {
-    try {
-     if (req.model.modelName == "bookings") {
-        // const id = req.params.id;
-        const record = await req.model.readAll( model.RoomModel,model.PaymentModel,model.ServiceModel,model.CustomerModel)
-        res.status(200).json(record);
-      } else if(req.model.modelName == "rooms" ){
-        const records = await req.model.findAll(
-          RoomFeatureModel
-        )
-        res.status(200).json(records);
-
-      }else if(req.model.modelName == "employee"){
-        const records = await req.model.findAll(
-          EmployeeRoleModel
-        )
-        res.status(200).json(records);
-
-      } 
-      // else if(req.model.modelName == "customer"){
-      //   const records = await req.model.findAll(
-      //     CustomerModel
-      //   )
-      //   res.status(200).json(records);
-
-      // }
-      else {
-        let allRecords = await req.model.get();
-        res.status(200).json(allRecords);
-
-      }
-     
-    } catch (err) {
-      next(err);
+  try {
+    if (req.model.modelName == "bookings") {
+      const record = await req.model.readAll(
+        model.RoomModel,
+        model.PaymentModel,
+        model.ServiceModel,
+        model.CustomerModel
+      );
+      res.status(200).json(record);
+    } else if (req.model.modelName == "rooms") {
+      const records = await req.model.findAll(RoomFeatureModel);
+      res.status(200).json(records);
+    } else if (req.model.modelName == "employee") {
+      const records = await req.model.findAll(EmployeeRoleModel);
+      res.status(200).json(records);
     }
-  }
 
-
-  async function handleGetOne(req, res, next) {
-    try {
-      const id = req.params.id;
-      if (req.model.modelName == "bookings") {
-        const record = await req.model.readOne(id, model.RoomModel,model.PaymentModel,model.ServiceModel, model.CustomerModel);
-        res.status(200).json(record);
-      }else if(req.model.modelName == "rooms" ){
-        const records = await req.model.findone(id,RoomFeatureModel )
-        res.status(200).json(records);
-
-      }else if(req.model.modelName == "employee"){
-        const records = await req.model.findone(id,EmployeeRoleModel)
-        res.status(200).json(records);
-
-      } 
-      // else if(req.model.modelName == "customer"){
-      //   const records = await req.model.findone(id,CustomerModel)
-      //   res.status(200).json(records);
-
-      // }
-      else {
-        let id = req.params.id;
-        let theRecord = await req.model.get(id);
-        res.status(200).json(theRecord);
-      }
-      
-
-    } catch (err) {
-      next(err);
+    else {
+      let allRecords = await req.model.get();
+      res.status(200).json(allRecords);
     }
+  } catch (err) {
+    next(err);
   }
-  
-  
-  async function handleCreate(req, res, next) {
-    try {
-      let newRecord = await req.model.create(req.body);
-      res.status(201).json(newRecord);
-    } catch (err) {
-      next(err);
+}
+
+async function handleGetOne(req, res, next) {
+  try {
+    const id = req.params.id;
+    if (req.model.modelName == "bookings") {
+      const record = await req.model.readOne(
+        id,
+        model.RoomModel,
+        model.PaymentModel,
+        model.ServiceModel,
+        model.CustomerModel
+      );
+      res.status(200).json(record);
+    } else if (req.model.modelName == "rooms") {
+      const records = await req.model.findone(id, RoomFeatureModel);
+      res.status(200).json(records);
+    } else if (req.model.modelName == "employee") {
+      const records = await req.model.findone(id, EmployeeRoleModel);
+      res.status(200).json(records);
     }
-  }
 
-  async function handleUpdate(req, res, next) {
-    try {
-      let updatedRecord = await req.model.update(req.params.id, req.body);
-      res.status(200).json(updatedRecord);
-    } catch (err) {
-      next(err);
+    else {
+      let id = req.params.id;
+      let theRecord = await req.model.get(id);
+      res.status(200).json(theRecord);
     }
+  } catch (err) {
+    next(err);
   }
+}
 
-  async function handleDelete(req, res, next) {
-    try {
-      let deletedRecord = await req.model.delete(req.params.id);
-      res.status(200).json(deletedRecord);
-    } catch (err) {
-      next(err);
-    }
+async function handleCreate(req, res, next) {
+  try {
+    let newRecord = await req.model.create(req.body);
+    res.status(201).json(newRecord);
+  } catch (err) {
+    next(err);
   }
+}
 
-  module.exports = router;
+async function handleUpdate(req, res, next) {
+  try {
+    let updatedRecord = await req.model.update(req.params.id, req.body);
+    res.status(200).json(updatedRecord);
+  } catch (err) {
+    next(err);
+  }
+}
 
+async function handleDelete(req, res, next) {
+  try {
+    let deletedRecord = await req.model.delete(req.params.id);
+    res.status(200).json(deletedRecord);
+  } catch (err) {
+    next(err);
+  }
+}
 
+module.exports = router;
