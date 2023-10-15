d7606c0c1e9093a374939bef594f32544a5d835b
Ayah-AQ <ayoya_1998@hotmail.com>
2023-10-15 13:53:08 +0000

    edit

--- src/models/index.js
+++ src/models/index.js
@@ -44,9 +44,13 @@ const CustomerModel = require("./users")(sequelize, DataTypes);
 const TourModel = require("./tour/tour")(sequelize, DataTypes);
 const GuideModel = require("./guide/guide")(sequelize, DataTypes);
 const tourCommnet = require("./tour/tourCommnet")(sequelize, DataTypes);
+const CommentRoom = require("./rooms/roomCommnet")(sequelize, DataTypes);
 
 GuideModel.hasMany(BookingModel, { foreignKey: "guide_id" });
 BookingModel.belongsTo(GuideModel, { foreignKey: "guide_id" });
+
+RoomModel.hasMany(CommentRoom, { foreignKey: "commnet_id" });
+CommentRoom.belongsTo(RoomModel, { foreignKey: "commnet_id" });
 
 TourModel.hasMany(tourCommnet, { foreignKey: "commnet_id" });
 tourCommnet.belongsTo(TourModel, { foreignKey: "commnet_id" });
@@ -119,7 +123,9 @@ module.exports = {
   guide: new DataCollection(GuideModel),
   tour: new DataCollection(TourModel),
   theTourCommnet:new DataCollection(tourCommnet),
+  commentRoom:new DataCollection(CommentRoom),
   tourCommnet,
+  CommentRoom,
   GuideModel,
   TourModel,
   CustomerModel,
